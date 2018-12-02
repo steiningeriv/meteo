e7d5965633bd3aa4cfa4fb160df50ec8ee862407
arischeidell <ans244@psu.edu>
2018-12-01 20:33:54 -0500

    Got date to dynamically load for weather widget

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function loadWeather() {
           }
           var condition = responseData.weather[0].main
           var icon = "images/" + responseData.weather[0].icon + ".svg";
+          var date = new Date();
+          console.log(date.getMonth() + " " + date.getDay() + ", " + date.getYear());
+          var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+          var months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+          document.getElementById("date").innerHTML = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDay();
 
           var fileName = location.pathname.split("/").slice(-1);
           document.getElementById("weather-icon").src = icon;
